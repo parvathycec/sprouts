64e6b95d66839d4df8527e71c763e4e6d071e562
parvathy <pmoha4@unh.newhaven.edu>
2018-11-01 12:39:43 -0400

    Text indicating which player has the turn and different color for player curves. Game starts text added.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+//Sprint 2: User's names
+var player1 = 'Player 1';//default name
+var player2 = 'Player 2';//default name
+var currentPlayer = player1;
+
 //list of circles in the canvas
-circles = []
+var circles = []
 
 //this function checks if the user has touched inside the circle
 function checkInCircle(x, y){
@@ -17,9 +22,11 @@ function checkInCircle(x, y){
 //creating the initial circles
 var circle1 = new Path.Circle(new Point(200, 150), 15);
 circle1.fillColor = 'red';
+circle1.visible = false;//Sprint 2
 circles.push(circle1)
 var circle2 = new Path.Circle(new Point(400, 300), 15);
 circle2.fillColor = 'red';
+circle2.visible = false;//Sprint 2
 circles.push(circle2)
 
 //defining the curve
@@ -37,14 +44,19 @@ function onMouseDown(event) {
 		path.selected = false;
 	}
 	console.log('event ', event.point)
-	// Create a new path and set its stroke color to black:
+	// Create a new path and set its stroke color to green for Player 1:
 	path = new Path({
 		segments: [event.point],
-		strokeColor: 'black',
 		strokeWidth: 6
 		// Select the path, so we can see its segment points:
 		//fullySelected: true
 	});
+    //Sprint 2: player based color 
+    if(currentPlayer == player1){
+        path.strokeColor = 'green';
+    }else{
+        path.strokeColor = 'darkblue';
+    }
 	console.log(path)
 }
 
@@ -84,5 +96,51 @@ function onMouseUp(event) {
 	var difference = segmentCount - newSegmentCount;
 	var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
 	path = 0;   
+    //Sprint 2: Player's Move
+    if(currentPlayer == player1){
+        textItem.fillColor = 'darkblue';
+        currentPlayer = player2;
+        textItem.content = player2 + "'s turn";
+    }else{
+        textItem.fillColor = 'green';   
+        currentPlayer = player1;
+        textItem.content = player1 + "'s turn";
+    }
+    }
+}
+
+//var rectForText = new Path.Rectangle(50,30,120,30);
+//rectForText.opacity = 0.5;
+//rectForText.fillColor = 'yellow';
+
+
+//Adding for Sprint 2: Text message saying the turn of the user.
+var textItem = new PointText({
+	content: player1 + "'s turn",
+    point: (20,30),
+	fillColor: 'green',
+    fontSize: 20,
+    fontWeight: 10,
+    visible: false,
+});
+//Sprint 2: Game starts text message
+var textGameStarts = new PointText({
+	content: "GAME STARTS",
+	justification: 'center',
+    point: view.center,
+	fillColor: 'green',
+    fontSize: 10,
+    fontWeight: 10
+});
+
+//Sprint 2: animation for game starts text
+function onFrame(event) {
+	textGameStarts.fillColor.hue += 1;
+    textGameStarts.fontSize += 0.5;
+    if(event.count == 120){
+        textGameStarts.visible = false;
+        circle1.visible = true;
+        circle2.visible = true;
+        textItem.visible = true;
     }
 }
